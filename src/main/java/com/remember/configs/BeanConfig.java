12e49cae8e62e24dab8631e85ce267e3fbfc09f2
DONGWON KIM <lunacircle4@likelion.org>
2022-07-03 18:22:13 +0900

    feat: BeanConfig 작성

---
+++ src/main/java/com/remember/configs/BeanConfig.java
@@ -0,0 +1,19 @@
+package com.remember.configs;
+
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
+@Configuration
+public class BeanConfig {
+    @PersistenceContext
+    private EntityManager em;
+
+    @Bean
+    public JPAQueryFactory jpaQueryFactory() {
+        return new JPAQueryFactory(em);
+    }
+}
